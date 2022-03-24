8239d1db85793413c49c117fc48f37d35b56dcc5
Vladislav Bondarenko <sunrineme@gmail.com>
2022-03-24 22:59:43 +0200

    some refactoring && add highlighting selected cell && correct empty cell creation
    Signed-off-by: Vladislav Bondarenko <sunrineme@gmail.com>

--- Pract4/zad_13.py
+++ Pract4/zad_13.py
@@ -7,39 +7,37 @@ class Pole:
 
     def __init__(self):
 
-        self.__pole = [
-            [],
-            [],
-            [],
-            [],
-        ]
-
-        av = list(range(1, 16))
-        rn1, rn2 = random.randint(0, 3), random.randint(0, 3)
-        self.__pole[rn1].insert(rn2, None)
+        self.__pole = [[None] * 4 for _ in range(4)]
+        av = list(range(1, 17))
 
         for i in range(0, 4):
             for j in range(0, 4):
-                try:
-                    self.__pole[i][j]
-                except IndexError:
-                    rn = random.choice(av)
-                    ind = av.index(rn)
-                    self.__pole[i].append(av.pop(ind))
+                rn = random.choice(av)
+                ind = av.index(rn)
+                p = av.pop(ind)
+                self.__pole[i][j] = p if p != 16 else None
 
     @property
     def pole(self):
         return self.__pole
 
-    def move(self, x1, y1, x2, y2) -> bool:
+    def get(self, coords: tuple):
+        return self.__pole[coords[0]][coords[1]] if coords is not None else None
+
+    def set(self, coords: tuple, value: int):
+        self.__pole[coords[0]][coords[1]] = value
+
+    def move(self, old_coords: tuple, new_coords: tuple) -> bool:
         try:
-            if self.__pole[x2][y2] is not None:
+            old = self.get(old_coords)
+            new = self.get(new_coords)
+            if new is not None:
                 return False
-            elif x2 - x1 != 0 and y2 - y1 != 0:
+            elif new_coords[0] - old_coords[0] != 0 and new_coords[1] - old_coords[1] != 0:
                 return False
             else:
-                self.__pole[x2][y2] = self.__pole[x1][y1]
-                self.__pole[x1][y1] = None
+                self.set(new_coords, old)
+                self.set(old_coords, None)
                 return True
         except IndexError:
             return False
@@ -56,18 +54,29 @@ class App(tk.Tk):
         self.__picked = None
         self._draw()
 
-    def _pickup(self, coords: tuple):
+    def _high(self, but: tk.Button):
+        if not but.tag:
+            but.configure(fg="gray")
+            but.tag = True
+        else:
+            but.configure(fg="black")
+            but.tag = False
+
+    def _pickup(self, coords: tuple, but: tk.Button):
         if self.__picked is None:
             self.__picked = coords
+            self._high(but)
             return
         elif self.__picked == coords:
             self.__picked = None
+            self._high(but)
             return
         else:
-            res = self.__pole.move(self.__picked[0], self.__picked[1], coords[0], coords[1])
+            res = self.__pole.move(self.__picked, coords)
             if res:
                 self.__picked = None
-            self._draw()
+                self._high(but)
+                self._draw()
 
     def _draw(self):
         for i in range(0, 4):
@@ -79,11 +88,14 @@ class App(tk.Tk):
                               borderwidth=1,
                               relief="solid",
                               activebackground="white",
-                              command=partial(self._pickup, (i, j,))
                               )
+                b.configure(command=partial(self._pickup, coords=(i, j), but=b))
+                b.tag = False
                 b.grid(row=i, column=j)
 
 
 if __name__ == '__main__':
     app = App()
     app.mainloop()
+    # pole = Pole()
+    # print(pole.pole)
